a8671bc78e7f34ca0d4df57679b93c97fe4eb9c5
hacker-flavio <theflavioherrera@gmail.com>
2023-10-24 14:37:21 -0700

    created desposit withdraw undo getBalance etc

--- doubleLinkedListProjects/doubleLinkedList/Stash.h
+++ doubleLinkedListProjects/doubleLinkedList/Stash.h
@@ -10,11 +10,50 @@
 
 using namespace std;
 
+struct Balance
+{
+    int amount;
+    Balance()
+    {
+        amount = 0;
+    }
+};
+
 template <class T>
 class Stash
 {
 private:
+    Balance balance;
+    Stack<int> history;
+
 public:
-}
+    void deposit(int value)
+    {
+        cout << "you deposited: " << value << endl;
+        history.push(value);
+        balance.amount += value;
+    }
+    void withdraw(int value)
+    {
+        balance.amount -= value;
+        history.push(-value);
+        cout << "you withdrew: " << value << endl;
+    }
+    void undo()
+    {
+        int value = history.pop();
+        balance.amount -= value;
+
+        cout << "you undid the last transaction" << endl;
+    }
+    int getBalance()
+    {
+        return balance.amount;
+    }
+    void printHistory()
+    {
+        history.elements.isEmpty() ? cout << "history is empty" << endl : cout << "history: " << history.elements << endl;
+    }
+};
 
 #endif
--- doubleLinkedListProjects/doubleLinkedList/main.cpp
+++ doubleLinkedListProjects/doubleLinkedList/main.cpp
@@ -1,30 +1,27 @@
 #include <iostream>
 #include "LinkedList.h"
 #include "Stack.h"
+#include "Stash.h"
 
 using namespace std;
 
 int main()
 {
 
-    LinkedList<int> myArray;
+    Stash<int> jar;
 
-    myArray.append(1);
-    myArray.append(2);
-    myArray.append(3);
-    myArray.append(4);
-
-    myArray.prepend(0);
-
-    myArray.removeFirst();
-    myArray.removeLast();
-    // myArray.removeLast();
-    // myArray.removeLast();
-    myArray.isEmpty() ? cout << "array is empty" << endl : myArray.size() == 1 ? cout << "there is one item left in the array" << endl
-                                                                               : cout << "array is not empty" << endl;
-
-    cout << myArray << endl;
-    cout << "array size equals: " << myArray.size() << endl;
+    jar.deposit(5);
+    cout << jar.getBalance() << endl;
+    jar.printHistory();
+    jar.withdraw(2);
+    cout << jar.getBalance() << endl;
+    jar.printHistory();
+    jar.undo();
+    cout << jar.getBalance() << endl;
+    jar.printHistory();
+    jar.undo();
+    cout << jar.getBalance() << endl;
+    jar.printHistory();
 
     return 0;
 }
